1f972735afde17da4448499ce201fb8d183e6f3b
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2012-03-19 17:33:42 +0000

    Add float type

--- Entity/Attribute.php
+++ Entity/Attribute.php
@@ -25,6 +25,7 @@ class RM_Entity_Attribute {
 			case 'string':
 				return (string)$value;
 			case 'decimal':
+			case 'float':
 				return $value - 0.0;
 			default:
 				throw new Exception("Unknow '{$this->_getProperties()->getFieldName()}' value type");
