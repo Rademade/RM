04c04dc37d233c06198c404f607f9f68322a48d9
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2012-12-15 12:27:13 +0000

    Update trait CM

--- Trait/Content.php
+++ Trait/Content.php
@@ -10,11 +10,19 @@ trait RM_Trait_Content {
 
     abstract protected function __setIdContent($idContent);
 
+    /**
+     * @param RM_Content $contentManager
+     * @return \RM_Trait_Content
+     */
     public function setContentManager(RM_Content $contentManager) {
         $this->__setIdContent( $contentManager->getId() );
         $this->_content = $contentManager;
+        return $this;
     }
 
+    /**
+     * @return RM_Content
+     */
     public function getContentManager() {
         if (!$this->_content instanceof RM_Content) {
             $this->_content = RM_Content::getById( $this->getIdContent() );
@@ -22,10 +30,16 @@ trait RM_Trait_Content {
         return $this->_content;
     }
 
+    /**
+     * @return RM_Content_Lang
+     */
     public function getDefaultContent() {
         return $this->getContentManager()->getDefaultContentLang();
     }
 
+    /**
+     * @return RM_Content_Lang
+     */
     public function getContent() {
         return $this->getContentManager()->getCurrentContentLang();
     }
