07a0dd4d2463f3a88d6d3ccddb3e81da0a0722e1
Vladislav Melanitskiy <vlad.melanitski@gmail.com>
2013-08-17 21:23:37 +0000

    Add sitemap object item

---
+++ System/Sitemap/ObjectItem.php
@@ -0,0 +1,13 @@
+<?php
+abstract class RM_System_Sitemap_ObjectItem {
+
+    private static $_urlHelper;
+
+    protected static function _getUrlHelper() {
+        if (!self::$_urlHelper instanceof Zend_View_Helper_Url) {
+            self::$_urlHelper = new Zend_View_Helper_Url();
+        }
+        return self::$_urlHelper;
+    }
+
+}
