77b715636c535e5fa41c7e8c9b0224a27b4124a6
Yaroslav Konoplev <the.yivo@gmail.com>
2013-11-29 14:33:38 +0000

    Update breadcrumb trait

---
+++ Admin/BreadCrumb.php
@@ -0,0 +1,34 @@
+<?php
+trait RM_Admin_BreadCrumb {
+
+    protected $_addTitle = 'add ';
+    protected $_editTitle = 'edit ';
+    protected $_listTitle = ' list';
+
+    protected $_addName;
+    protected $_titleName;
+
+    protected function getListCrumbName() {
+        return ucfirst($this->_listTitle) . $this->_getAddName();
+    }
+
+    protected function getAddCrumbName() {
+        return ucfirst($this->_addTitle) . $this->_getAddName();
+    }
+
+    protected function getEditCrumbName() {
+        return ucfirst($this->_editTitle) . $this->_getAddName();
+    }
+
+    protected function _getAddName() {
+        $name = is_string($this->_addName) ? $this->_addName : $this->_titleName;
+        return mb_strtolower($name, 'utf-8');
+    }
+
+    protected function __setTitle($title) {
+        $this->_titleName = $title;
+        RM_View_Top::getInstance()->setTitle( $title );
+        $this->view->headTitle($title); # =(
+    }
+
+}
