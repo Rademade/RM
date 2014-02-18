91e36e35b8a6d2edd3a94f567693b4f909b039bb
Yaroslav Konoplov <the.yivo@gmail.com>
2014-02-18 17:17:25 +0200

    added footer pages to public controller

--- System/Controller/Public.php
+++ System/Controller/Public.php
@@ -20,18 +20,21 @@ abstract class RM_System_Controller_Public
      * @var RM_User_Session
      */
     protected $_userSession;
+ protected $_footerPages;
 
-	public function preDispatch() {
+    public function preDispatch() {
         parent::preDispatch();
 		$this->_cart = Application_Model_User_Cart::getInstance();
 		$this->_userWatcher = Application_Model_Watcher::getInstance();
         $this->_userSession = RM_User_Session::getInstance();
+        $this->_footerPages = Application_Model_Page::getFooterPages();
     }
 
 	public function postDispatch() {
         parent::postDispatch();
 		$this->view->assign(array(
-		    'cart' => $this->_cart
+		    'cart' => $this->_cart,
+            'footerItems' => $this->_footerPages
 		));
 	}
 
