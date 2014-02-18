5eed1a52aad54d36c655b594edbe6ed7df0a4ba5
Sergey Kuzhavskiy <kuzh@rademade.com>
2014-02-18 17:23:35 +0200

    removed footer pages from public controller

--- System/Controller/Public.php
+++ System/Controller/Public.php
@@ -20,21 +20,18 @@ abstract class RM_System_Controller_Public
      * @var RM_User_Session
      */
     protected $_userSession;
- protected $_footerPages;
 
     public function preDispatch() {
         parent::preDispatch();
 		$this->_cart = Application_Model_User_Cart::getInstance();
 		$this->_userWatcher = Application_Model_Watcher::getInstance();
         $this->_userSession = RM_User_Session::getInstance();
-        $this->_footerPages = Application_Model_Page::getFooterPages();
     }
 
 	public function postDispatch() {
         parent::postDispatch();
 		$this->view->assign(array(
-		    'cart' => $this->_cart,
-            'footerItems' => $this->_footerPages
+		    'cart' => $this->_cart
 		));
 	}
 
