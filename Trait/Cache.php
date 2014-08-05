01913079d2750f7f26e6b891cb3e244627c7093e
Sergey Kuzhavskiy <kuzh@rademade.com>
2014-08-05 13:44:35 +0300

    updated cache class

--- Trait/Cache.php
+++ Trait/Cache.php
@@ -1,36 +1,47 @@
 <?php
 trait RM_Trait_Cache {
 
-    private $_traitCacheManager;
+    private static $_traitCacheManager;
 
-    protected function __getCachedValue($key, Closure $function) {
-        $validKey = $this->_removeInvalidCharacters($key);
-        $cachedValue = $this->_getCache()->load($validKey);
+    protected static function __getCachedValue($key, Closure $function) {
+        $validKey = self::_removeInvalidCharacters($key);
+        $cachedValue = self::_getCache()->load($validKey);
         if ($cachedValue === false) {
             $cachedValue = $function();
-            $this->_getCache()->save($cachedValue, $validKey);
+            self::_getCache()->save($cachedValue, $validKey);
         }
         return $cachedValue;
     }
 
-    protected function __cleanCachedValue($key) {
-        $validKey = $this->_removeInvalidCharacters($key);
-        $this->_getCache()->remove($validKey);
+    protected static function __cleanCachedValue($key) {
+        $validKey = self::_removeInvalidCharacters($key);
+        self::_getCache()->remove($validKey);
     }
 
-    protected function __getCacheName() {
-        return 'default';
-    }
-
-    private function _removeInvalidCharacters($key) {
+    private static function _removeInvalidCharacters($key) {
         return preg_replace('/([^a-zA-Z0-9_])/', '', $key);
     }
 
-    private function _getCache() {
-        if (!$this->_traitCacheManager instanceof Zend_Cache_Core) {
-            $this->_traitCacheManager = Zend_Registry::get('cachemanager')->getCache($this->__getCacheName());
+    /**
+     * @return Zend_Cache_Core
+     */
+    private static function _getCache() {
+        if (!self::$_traitCacheManager instanceof Zend_Cache_Core) {
+            self::$_traitCacheManager = Zend_Registry::get('cachemanager')->getCache(static::_getCacheName());
         }
-        return $this->_traitCacheManager;
+        return self::$_traitCacheManager;
+    }
+
+    protected static function _getCacheName() {
+        return 'default';
+    }
+
+    /**
+     * @deprecated
+     * @return string
+     */
+    protected function __getCacheName() {
+       return $this->_getCacheName();
     }
 
 }
