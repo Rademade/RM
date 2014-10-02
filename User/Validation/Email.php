a590040b0fbffd8d5d3d1d99f319bfef67dbb2c4
Yaroslav Konoplov <the.yivo@gmail.com>
2014-10-02 16:46:54 +0300

    little refactored

--- User/Validation/Email.php
+++ User/Validation/Email.php
@@ -3,9 +3,9 @@ class RM_User_Validation_Email
 	extends
 		RM_User_Validation {
 
-    const EMAIL_EXCEPTION_CODE = 100;
-
     const CACHE_NAME = 'emailValidation';
+
+    const EMAIL_EXCEPTION_CODE = 100;
 
 	private $_email;
 
@@ -18,29 +18,17 @@ class RM_User_Validation_Email
 	}
 
 	public function isValid() {
-		$validator = new Zend_Validate_EmailAddress(array(
-	        'allow' => Zend_Validate_Hostname::ALLOW_DNS,
-		));
-		return $validator->isValid( $this->getEmail() );
+        return preg_match('/.+@.+\..+/i', $this->getEmail());
 	}
 
     public function isValidRemote() {
-        return $this->isValid() && $this->_checkRemoteEmail( $this->getEmail() );
+        return $this->_validateWithOnlineService($this->getEmail());
     }
 
-	public function isUnique( $excludedId = 0 ) {
-		if ($this->isValid()) {
-            $profileClass = RM_Dependencies::getInstance()->userProfile;
-			$user = $profileClass::getByEmail( $this->getEmail() );
-			if ($user instanceof $profileClass) {
-				if ($user->getId() !== $excludedId) {
-					return false;
-				}
-			}
-			return true;
-		} else {
-			return false;
-		}
+	public function isUnique($excludedId = 0) {
+        $profileClass = RM_Dependencies::getInstance()->userProfile;
+        $user = $profileClass::getByEmail( $this->getEmail() );
+        return !$user instanceof $profileClass || $user->getId() !== $excludedId;
 	}
 
 
@@ -51,8 +39,8 @@ class RM_User_Validation_Email
 		);
 	}
 
-    private function _checkRemoteEmail($email) {
-        $emailHash = md5( $email );//generate hash
+    private function _validateWithOnlineService($email) {
+        $emailHash = md5( $email ); //generate hash
         /* @var Zend_Cache_Manager $cachemanager */
         $cachemanager = Zend_Registry::get('cachemanager');
         /* @var Zend_Cache_Core $cache */
@@ -60,10 +48,10 @@ class RM_User_Validation_Email
         if ( ($status = $cache->load( $emailHash )) === false ) {
             $browser = new RM_System_Browser();
             $browser->setMaxWaiting( 30 );
-            $browser->setPostData( array(
+            $browser->setPostData([
                 'name' => 'email',
                 'cmd' => $email
-            ) );
+            ]);
             $result = $browser->download( 'http://domw.net/data.php' );
             $status = preg_match('/OK\</', $result);
             $cache->save( $status );
