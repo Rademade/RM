20df48a51fbbb8f271c94ff566ecbdebacca5f4f
Sergey Kuzhavskiy <kuzh@rademade.com>
2014-12-23 13:10:40 +0200

    extracted method form text field

--- View/Form/Field/Text.php
+++ View/Form/Field/Text.php
@@ -1,35 +1,39 @@
 <?php
 class RM_View_Form_Field_Text
-	extends
+    extends
         RM_View_Form_Field {
-	
-	private $type = 'text';
-	
-	const TPL = 'text.phtml';
-		
-	public function __construct($desc, $name, $value = null) {
-		parent::__construct($name, $desc, $value);
-	}
 
-	public function getType() {
-		return $this->type;
-	}
+    private $type = 'text';
 
-	public function setType($type) {
-		$this->type = $type;
-		return $this;
-	}
+    const TPL = 'text.phtml';
 
-	public function render($idLang) {
-		$row = new RM_View_Form_Row();
-		$row->setDesc( $this->getDesc() );
-		$row->setHTML( $this->getView()->partial(
-			static::BASE_PATH . static::TPL,
-			$this->addFieldData($idLang, array(
-				'type' => $this->getType()
-			) )
-		));
-		return $this->renderRow( $row );
-	}
-	
+    public function __construct($desc, $name, $value = null) {
+        parent::__construct($name, $desc, $value);
+    }
+
+    public function getType() {
+        return $this->type;
+    }
+
+    public function setType($type) {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function render($idLang) {
+        $row = new RM_View_Form_Row();
+        $row->setDesc($this->getDesc());
+        $row->setHTML($this->getView()->partial(
+            static::BASE_PATH . static::TPL,
+            $this->__getPartialData($idLang)
+        ));
+        return $this->renderRow($row);
+    }
+
+    protected function __getPartialData($idLang) {
+        return $this->addFieldData($idLang, array(
+            'type' => $this->getType()
+        ));
+    }
+
 }
