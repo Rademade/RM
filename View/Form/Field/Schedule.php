a949283a4e332f9f7fe955097fe59ff66c807e32
masha-feilyk <mf@rademade.com>
2017-06-27 20:23:45 +0300

    add schedule layout in admin

--- View/Form/Field/Schedule.php
+++ View/Form/Field/Schedule.php
@@ -1,7 +1,30 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: mariia
- * Date: 6/27/17
- * Time: 6:16 PM
- */
+class RM_View_Form_Field_Schedule
+    extends RM_View_Form_Field {
+
+    private $_options = array();
+    private $_selected = array();
+
+    const TPL = 'schedule.phtml';
+
+    public function __construct(
+        $desc,
+        $name
+    ) {
+        parent::__construct($name, $desc, '');
+    }
+
+    public function render($idLang) {
+        $row = new RM_View_Form_Row();
+        $row->setDesc( $this->getDesc() );
+        $row->setHTML( $this->getView()->partial(
+            self::BASE_PATH . self::TPL,
+            $this->addFieldData($idLang, array(
+                'options' => $this->_options,
+                'selected' => $this->_selected
+            ))
+        ) );
+        return $this->renderRow($row);
+    }
+
+}
