0f2bc0240e848ccc50aec9b6a380fcb8749600de
xfearx <xfearx@ukr.net>
2017-07-10 10:22:13 +0300

    Schedule component

--- View/Form/Field/Schedule.php
+++ View/Form/Field/Schedule.php
@@ -1,6 +1,7 @@
 <?php
 class RM_View_Form_Field_Schedule
-    extends RM_View_Form_Field {
+    extends 
+        RM_View_Form_Field {
 
     private $_options = array();
     private $_selected = array();
@@ -8,22 +9,26 @@ class RM_View_Form_Field_Schedule
     const TPL = 'schedule.phtml';
 
     public function __construct(
-        $desc,
-        $name
+        $name,
+        $postName,
+        $availableValues
     ) {
-        parent::__construct($name, $desc, '');
+        parent::__construct($postName, $name, '');
+        Head::getInstance()->getJS()->add('schedule');
+        $this->_selected = $postName;
+        $this->_options = $availableValues;
     }
 
     public function render($idLang) {
         $row = new RM_View_Form_Row();
-        $row->setDesc( $this->getDesc() );
-        $row->setHTML( $this->getView()->partial(
+        $row->setDesc($this->getDesc());
+        $row->setHTML($this->getView()->partial(
             self::BASE_PATH . self::TPL,
             $this->addFieldData($idLang, array(
                 'options' => $this->_options,
                 'selected' => $this->_selected
             ))
-        ) );
+        ));
         return $this->renderRow($row);
     }
 
